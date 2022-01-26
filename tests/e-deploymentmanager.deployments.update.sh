a80e2f56b4996a2d6779dbd162a7e14ec6e0d646
Carlos.Martin <carlos.martin@mettle.co.uk>
2022-01-26 16:46:03 +0000

    e

--- tests/e-deploymentmanager.deployments.update.sh
+++ tests/e-deploymentmanager.deployments.update.sh
@@ -3,13 +3,29 @@
 source lib/lib.sh
 
 echo "================================================"
-echo "Checking deploymentmanager.deployments.create"
+echo "Checking deploymentmanager.deployments.update"
 
 enable_service "deploymentmanager.googleapis.com"
 
-modify_role "deploymentmanager.deployments.create"
+modify_role "deploymentmanager.deployments.update"
 
 # More resources https://cloud.google.com/deployment-manager/docs/configuration/supported-resource-types
+echo "resources:
+- name: vm-created-by-deployment-manager
+  type: compute.v1.instance
+  properties:
+    zone: us-central1-a
+    machineType: zones/us-central1-a/machineTypes/n1-standard-1
+    disks:
+    - deviceName: boot
+      type: PERSISTENT
+      boot: true
+      autoDelete: true
+      initializeParams:
+        sourceImage: projects/debian-cloud/global/images/family/debian-9
+    networkInterfaces:
+    - network: global/networks/test" > /tmp/gcp_privesc1.yaml
+  
 echo "resources:
 - name: vm-created-by-deployment-manager
   type: compute.v1.instance
@@ -28,10 +44,15 @@ echo "resources:
       initializeParams:
         sourceImage: projects/debian-cloud/global/images/family/debian-9
     networkInterfaces:
-    - network: global/networks/test" > /tmp/gcp_privesc.yaml
+    - network: global/networks/test" > /tmp/gcp_privesc2.yaml
+
+gcloud deployment-manager deployments create test-vm-deployment --config /tmp/gcp_privesc1.yaml
+
+echo "Sleeping 1min to then update deployment"
+sleep 60
 
 gcloud --impersonate-service-account="${SERVICE_ACCOUNT_ID}@${PROJECT_ID}.iam.gserviceaccount.com"\
-  deployment-manager deployments create test-vm-deployment --config /tmp/gcp_privesc.yaml
+  deployment-manager deployments update test-vm-deployment --config /tmp/gcp_privesc2.yaml
 
 # Cleaning
 gcloud deployment-manager deployments list
@@ -39,7 +60,7 @@ echo "Access: https://console.cloud.google.com/dm/deployments?project=${PROJECT_
 read -p "Press any key to delete scenario... " -n1 -s
 echo ""
 
-rm /tmp/gcp_privesc.yaml
+rm /tmp/gcp_privesc1.yaml /tmp/gcp_privesc2.yaml
 
 modify_role "deploymentmanager.deployments.delete"
 
